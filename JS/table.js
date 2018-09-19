2e88bc691f760eb4840e7110caa60ac8ca0b8009
birukyemane <bir_yem@yahoo.com>
2018-09-20 02:38:10 +0300

    setting styles separated from creating the table .  generate table and then set styles in the main.js

--- JS/table.js
+++ JS/table.js
@@ -1,32 +1,22 @@
 // returns html code of the table generated 
-function createTable(styles){  
-  let tableStyle = `"width:${styles.tableWidth}% ; border:${styles.borderWidth}px solid 
-                    ${styles.borderColor}; border-collapse:collapse; background:${styles.tableBackground}"`;
-  return `<table id="tableID" style=${tableStyle}>  
-            ${createHeaders(styles)} 
-            <tbody style="color:${styles.fontColor}">
-             ${createRows(styles)}
-            </tbody>
-         </table>`; // add rows to the table
+function createTable(rows,columns){  
+  //let tableStyle = `"width:${styles.tableWidth}% ; border:${styles.borderWidth}px solid 
+   //                 ${styles.borderColor}; border-collapse:collapse; background:${styles.tableBackground}"`;
+  return `<table> <thead> ${createHeaders(columns)} </thead> <tbody> ${createRows(rows,columns)}  </tbody>  </table>`; // add rows to the table
 }
 
-function createHeaders(styles){
+function createHeaders(columns){
   let header = '';
-  let headerStyle = `background:${styles.headerBackground}`;
-  for(let i=1;i <= styles.columns;i++){
-    header += `<th style=${headerStyle}>Head${i}</th>`;
+  for(let i=1;i <= columns;i++){
+    header += `<th>Head${i}</th>`;
   }
-  //return header;
-  console.log('header is', header,'coulumns are', styles.columns)
-  return `<tr> ${header}</tr>`;
+  return `<tr">${header}</tr>`;  
 }
 
-function createRows(styles){
-  let row = `<tr> ${createCells(styles)}</tr>`;  
-  return row.repeat(Number(styles.rows)); 
+function createRows(rows,columns){
+  return  `<tr> ${createCells(columns)}</tr>`.repeat(Number(rows)); 
 }
 
-function createCells(styles){
-  let cell = `<td>value</td>`;
-  return cell.repeat(Number(styles.columns)); 
+function createCells(columns){
+  return `<td>value</td>`.repeat(Number(columns)); 
 }
